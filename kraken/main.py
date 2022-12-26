de0654822d8935e9d99c84d3af3f60aa978ac087
Victor Madureri <41347134+vmmq@users.noreply.github.com>
2022-12-26 13:22:53 -0800

    formatter

--- kraken/main.py
+++ kraken/main.py
@@ -6,58 +6,51 @@ import pandas as pd
 
 api = KrakenApi()
 
-pairs =  api.get_tradable_assets_pais()
+pairs = api.get_tradable_assets_pais()
 
-interval_display_options = (
-        '1 min',
-        '5 min',
-        '15 min',
-        '30 min',
-        '1 hour',
-        '4 hours',
-        '1 day',
-        '1 week',
-        '2 weeks', 
-        )
-
-interval_value_options = (
-        1,
-        5,
-        15,
-        30,
-        60,
-        240,
-        1440,
-        10080,
-        21600,
-        )
-
-format_function = lambda x: interval_display_options[interval_value_options.index(x)]
+key_values = {
+    1: "1 min",
+    5: "5 min",
+    15: "15 min",
+    30: "30 min",
+    60: "1 hour",
+    240: "4 hour",
+    1440: "1 day",
+    10080: "1 week",
+    21600: "2 weeks",
+}
 
 with st.sidebar:
     interval = st.sidebar.selectbox(
-        "Select chart interval", 
-        interval_value_options,
-        format_func= format_function
+        "Select chart interval",
+        options=key_values.keys(),
+        format_func=lambda x: key_values.get(x),
     )
 
 st.title("Kraken API")
 st.header("Kraken tradable asset pairs")
- 
-selected_pair = st.selectbox('Select the trade pair', pairs['wsname'])
 
-time.sleep(1) 
+selected_pair = st.selectbox("Select the trade pair", pairs["wsname"])
+
+time.sleep(1)
 
 ohlc_data = api.get_ohlc_data(selected_pair, interval=interval)
-ohlc_data['time'] = pd.to_datetime(ohlc_data['time'], unit='s', origin='unix').dt.date
+ohlc_data["time"] = pd.to_datetime(ohlc_data["time"], unit="s", origin="unix").dt.date
 fig = go.Figure()
-fig.add_trace(go.Candlestick(x=ohlc_data['time'], open=ohlc_data['open'], high=ohlc_data['high'], low=ohlc_data['low'], close=ohlc_data['close']) )
+fig.add_trace(
+    go.Candlestick(
+        x=ohlc_data["time"],
+        open=ohlc_data["open"],
+        high=ohlc_data["high"],
+        low=ohlc_data["low"],
+        close=ohlc_data["close"],
+    )
+)
 
 with st.container():
-    st.write('Candlestick chart')
+    st.write("Candlestick chart")
     st.plotly_chart(fig, use_container_width=True)
 
 st.header(ohlc_data.columns)
 
 st.dataframe(ohlc_data)
-
